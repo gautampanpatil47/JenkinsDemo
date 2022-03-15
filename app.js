4099d76405129fc72c6d42bc085dca967f8fb1aa
Panpatil <gpp921994@tatatechnologies.com>
2022-03-15 13:22:45 +0530

    New changes

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
-var http = require("http");
+var express = require('express')
+var app = express()
 
-http.createServer(function (request, response) {
-   // Send the HTTP header 
-   // HTTP Status: 200 : OK
-   // Content Type: text/plain
-   response.writeHead(200, {'Content-Type': 'text/plain'});
-   
-   // Send the response body as "Hello World"
-   response.end('Hello India\n');
-}).listen(8081);
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/public'))
 
-// Console will print the message
-console.log('Server running at http://127.0.0.1:8081/');
+app.get('/', function(request, response) {
+  response.send('Hello Testing DevOps Node! JS Project Hello How are you ? welcome to devOps Project')
+}) 
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
